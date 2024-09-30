893e87135f83e86ab30c87f8c752806cf1a11f85
billionquote <shahrakesh45@gmail.com>
2024-09-30 16:51:33 +0530

    Update worker.py

--- simbatolls/worker.py
+++ simbatolls/worker.py
@@ -5,9 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-# redis_url = os.getenv('REDIS_URL');
+redis_url = os.getenv('REDIS_URL');
 
-redis_url = os.getenv('REDIS_URL', 'rediss://:pf6c22c0fcedac7b44568fa823d496690a9624f061d7e1797f52d38f31d48294c@ec2-23-21-87-215.compute-1.amazonaws.com:12819')
+# redis_url = os.getenv('REDIS_URL', 'rediss://:pf6c22c0fcedac7b44568fa823d496690a9624f061d7e1797f52d38f31d48294c@ec2-23-21-87-215.compute-1.amazonaws.com:12819')
 
 conn = redis.from_url(redis_url, ssl_cert_reqs=None)
 # conn = Redis(host='localhost', port=6379)
